462e5e90f1831ab1220d4d63ea208dc73fc786c7
arathorn2 <efohughes@gmail.com>
2022-02-16 18:10:07 -0500

    e

--- src/main/scala/Location.scala
+++ src/main/scala/Location.scala
@@ -1,1 +1,15 @@
-case class Location(x: Int, y: Int) {}
+case class Location(x: Int, y: Int) {
+  def findAdjacents(): List[Location] = {
+    val adj = List(
+      Location(x, y - 1),
+      Location(x, y + 1),
+      Location(x - 1, y),
+      Location(x + 1, y),
+      Location(x - 1, y - 1),
+      Location(x - 1, y + 1),
+      Location(x + 1, y - 1),
+      Location(x + 1, y + 1)
+    )
+    adj
+  }
+}
