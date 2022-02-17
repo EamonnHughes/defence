31a6e70635f86914c5b34ec1c3f6d3c330978ab6
arathorn2 <efohughes@gmail.com>
2022-02-17 18:50:36 -0500

    Update World.scala

--- src/main/scala/World.scala
+++ src/main/scala/World.scala
@@ -3,7 +3,16 @@ object World {
     List(
       Squad(10, 4, Location(16, 16), 0, Location(16, 16)),
       Squad(10, 4, Location(16, 14), 0, Location(16, 14)),
-      Squad(10, 4, Location(16, 12), 0, Location(16, 12))
+      Squad(10, 4, Location(16, 12), 0, Location(16, 12)),
+      Squad(10, 4, Location(16, 10), 0, Location(16, 10)),
+      Squad(10, 4, Location(16, 8), 0, Location(16, 8)),
+      Squad(10, 4, Location(16, 6), 0, Location(16, 6)),
+      Squad(10, 4, Location(16, 15), 0, Location(16, 15)),
+      Squad(10, 4, Location(16, 13), 0, Location(16, 13)),
+      Squad(10, 4, Location(16, 11), 0, Location(16, 11)),
+      Squad(10, 4, Location(16, 9), 0, Location(16, 9)),
+      Squad(10, 4, Location(16, 7), 0, Location(16, 7)),
+      Squad(10, 4, Location(16, 5), 0, Location(16, 5))
     )
 
   var terrain = List(
