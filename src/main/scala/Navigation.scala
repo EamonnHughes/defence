6abd5465156f9958d12f3a1afd65fa3a03c04dd3
arathorn2 <efohughes@gmail.com>
2022-02-16 19:09:08 -0500

    Update Navigation.scala

--- src/main/scala/Navigation.scala
+++ src/main/scala/Navigation.scala
@@ -1,6 +1,14 @@
+import sun.security.ec.point.ProjectivePoint.Mutable
+
+import scala.collection.mutable
+
 object Navigation {
   def makePaths(start: Location, finish: Location): Path = {
+    var visitedCells = mutable.Set.empty[Location]
     var paths = List(Path(List(start)))
 
   }
+
+  def extendPaths(path: Path, visCells: List[Location]): List[Path] = {}
+
 }
