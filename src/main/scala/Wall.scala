7ee0ac30f4d90d78f45522e7481a7e5d1d1b0058
arathorn2 <efohughes@gmail.com>
2022-03-26 13:31:55 -0400

    Update Wall.scala

--- src/main/scala/Wall.scala
+++ src/main/scala/Wall.scala
@@ -1,5 +1,12 @@
+import processing.core.PApplet
+
 case class Wall(
     location: Location,
     width: Int,
     height: Int
-) extends Construction {}
+) extends Construction {
+  def draw(p: PApplet): Unit = {
+    p.fill(200, 200, 200)
+    p.rect(location.x * 16, location.y * 16, width * 16, height * 16)
+  }
+}
